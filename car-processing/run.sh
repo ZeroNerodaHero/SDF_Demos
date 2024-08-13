36f7249b8ae4ad0732f1beb6f0b1ee44bc2385e5
billy <billyz404meter@gmail.com>
2024-08-13 09:59:45 -0700

    containerized carproc

---
+++ car-processing/run.sh
@@ -0,0 +1,7 @@
+fluvio cluster start
+cd /workspace/connectors/ 
+cdk deploy start --ipkg infinyon-http-source-0.3.8.ipkg -c license-connector.yaml
+cdk deploy start --ipkg infinyon-http-source-0.3.8.ipkg -c car-connector.yaml
+cd /workspace
+sdf run --ephemeral
+fluvio consume cars
