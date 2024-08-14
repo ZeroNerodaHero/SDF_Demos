e7956d2b3ced86290bd4f9da318f41c788777de3
eve <billyz404meter@gmail.com>
2024-08-14 09:41:22 -0700

    fixed issue with sdf not running properly because of the dataflow.yaml. working on running sdf and consume command

--- car-processing/run.sh
+++ car-processing/run.sh
@@ -1,7 +1,8 @@
 fluvio cluster start
+fluvio cluster status
 cd /workspace/connectors/ 
 cdk deploy start --ipkg infinyon-http-source-0.3.8.ipkg -c license-connector.yaml
 cdk deploy start --ipkg infinyon-http-source-0.3.8.ipkg -c car-connector.yaml
 cd /workspace
-sdf run --ephemeral
-fluvio consume cars
+sdf run --ephemeral 
+fluvio consume speeding
